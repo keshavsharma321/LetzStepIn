1945fec46910e4036988de0ab96050447b9b0013
Keshav Sharma <keshavsharma5676@gmail.com>
2024-02-21 00:59:43 +0530

    U

--- pages/B4.tsx
+++ pages/B4.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const B4 = () => {
     return (
         <div>
-            <h1 className='flex justify-center text-5xl md:text-6xl p-4 md:p-0 mt-4 md:mt-4'>Sagar Ratna</h1>
+            <h1 className='flex justify-center text-5xl md:text-6xl p-4 md:p-0 mt-4 md:mt-4'>Phulwari Resort</h1>
             <div className='w-full lg:px-20 lg:py-24 flex  flex-col lg:flex-row'>
-                <div className='w-full m-4 lg:w-1/2'><img className='h-96' src="/Categories/Banquettes/image5"></img></div>
-                <div className='w-full  lg:w-1/2 justify-center text-center items-center flex'><p className='text-xl mt-10 sm:mt-0 p-4 sm:p-0 sm:text-3xl md:ml-10'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  </p></div>
+                <div className='w-full m-4 lg:w-1/2'><img className='h-96' src="/Categories/Banquettes/image4"></img></div>
+                <div className='w-full  lg:w-1/2 justify-center text-center items-center flex'><p className='text-xl mt-10 sm:mt-0 p-4 sm:p-0 sm:text-3xl md:ml-10'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industries standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.  </p></div>
             </div>
             <div className='px-16'>
                 <h1 className='flex justify-center p-16 md:ml-10 text-5xl md:text-6xl '>Aminities</h1>
